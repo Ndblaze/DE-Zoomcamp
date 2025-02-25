fa83b32ace339cca8e18882fd21b4971d68c9ca9
Ndubuisi Eze <ndubuisieze60@gmail.com>
2025-02-25 19:00:25 +0000

    finished all the questions

--- analytics-engineering/taxi_rides_ny/models/core/fct_fhv_monthly_zone_traveltime_p90.sql
+++ analytics-engineering/taxi_rides_ny/models/core/fct_fhv_monthly_zone_traveltime_p90.sql
@@ -1,19 +1,22 @@
+
 {{
     config(
         materialized='table'
     )
 }}
 
-WITH fhv_tripdata AS (
-    SELECT *, TIMESTAMP_DIFF(dropOff_datetime, pickup_datetime, DAY) as trip_duration
+with fhv_tripdata as (
+    SELECT
+        *,
+        TIMESTAMP_DIFF(dropOff_datetime, pickup_datetime, SECOND) AS trip_duration_seconds
     FROM {{ ref('dim_fhv_trips') }}
 )
 
-SELECT 
+SELECT
     year,
     month,
-    pickup_locationid,
-    dropoff_locationid,
-    PERCENTILE_CONT(trip_duration, 0.90) OVER (PARTITION BY year, month, pickup_locationid, dropoff_locationid) AS p90
+    pickup_zone,
+    dropoff_zone,
+    PERCENTILE_CONT(trip_duration_seconds, 0.90) OVER (PARTITION BY year, month, pickup_locationid, dropoff_locationid) AS p90
 FROM fhv_tripdata
 ORDER BY year, month
