cdba08643af17d6c751b817a0792562339818e74
Ndubuisi Eze <ndubuisieze60@gmail.com>
2025-02-25 17:03:27 +0000

    built my percentil, now i want to test it

--- analytics-engineering/taxi_rides_ny/models/core/fct_taxi_trips_monthly_fare_p95.sql
+++ analytics-engineering/taxi_rides_ny/models/core/fct_taxi_trips_monthly_fare_p95.sql
@@ -11,19 +11,18 @@ WITH valid_trips AS (
     FROM {{ ref("fact_trips") }}
     WHERE fare_amount > 0 
       AND trip_distance > 0 
-      AND payment_type IN (1, 2) -- Assuming 1 = 'Credit Card', 2 = 'Cash'
+      AND payment_type_description IN ('Cash', 'Credit Card')
 )
 
-SELECT DISTINCT
+SELECT 
     service_type,
     year,
     month,
-    PERCENTILE_CONT(0.97) WITHIN GROUP (ORDER BY fare_amount) 
-        OVER (PARTITION BY service_type, year, month) AS p97,
-    PERCENTILE_CONT(0.95) WITHIN GROUP (ORDER BY fare_amount) 
-        OVER (PARTITION BY service_type, year, month) AS p95,
-    PERCENTILE_CONT(0.90) WITHIN GROUP (ORDER BY fare_amount) 
-        OVER (PARTITION BY service_type, year, month) AS p90
+    PERCENTILE_CONT(fare_amount, 0.97) OVER (PARTITION BY service_type, year, month) AS p97,
+    PERCENTILE_CONT(fare_amount, 0.95) OVER (PARTITION BY service_type, year, month) AS p95,
+    PERCENTILE_CONT(fare_amount, 0.90) OVER (PARTITION BY service_type, year, month) AS p90
 FROM valid_trips
+GROUP BY service_type, year, month, fare_amount
 ORDER BY service_type, year, month
+
 
