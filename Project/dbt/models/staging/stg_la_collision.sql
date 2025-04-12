08cac76c8523ece62b30ec887a36b48a0e423fe4
Ndubuisi Eze <ndubuisieze60@gmail.com>
2025-04-12 20:10:53 +0000

    want to upload all the records now

--- Project/dbt/models/staging/stg_la_collision.sql
+++ Project/dbt/models/staging/stg_la_collision.sql
@@ -72,6 +72,7 @@ select
 from traffic_collisions
 where rn = 1
 
+-- dbt build --select <model_name> --vars '{'is_test_run': 'false'}'
 {% if var('is_test_run', default=true) %}
   limit 100
 {% endif %}
